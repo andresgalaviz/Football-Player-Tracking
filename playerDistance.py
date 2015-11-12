f033e0ca923dd92640996657f54e09a50425a056
davidzy08 <davidzy08@gmail.com>
2015-11-12 23:46:02 +0800

    use fake player to debug computing distance

--- playerDistance.py
+++ playerDistance.py
@@ -1,20 +1,29 @@
 import cv2
 import cv2.cv as cv
 import numpy as np
+import math
 
 videoName = 'panoramaFull1.avi'
-
+numOfPlayers = 2
 def getDistance(point1, point2):
-    return math.sqrt(np.power(point1 - point2, 2).sum())
+    p1x = point1[0]
+    p1y = point1[1]
+    p2x = point2[0]
+    p2y = point2[1]
+    return math.sqrt((p1x - p2x) * (p1x - p2x) + (p1y - p2y) * (p1y - p2y))
 
 # get the player list
 # to be implemented
 # use cv.GoodFeaturesToTrack to get fake players first
 def getplayers(frame):
-    #Convert to gray
-    cv.CvtColor(frame, curFrame, cv.CV_BGR2GRAY) 
-    return prePlayers = cv.GoodFeaturesToTrack(frame, None, None, 22, 0.1, 1)
-    
+    return cv.GoodFeaturesToTrack(frame, None, None, numOfPlayers, 0.1, 1)
+
+
+# use fake points for testing
+def fakePlayer():
+    return [(165,105), (10, 53)]
+
+
 def main():
     capture = cv.CaptureFromFile(videoName)
 
@@ -32,7 +41,7 @@ def main():
     curPyr = cv.CreateImage((height / 3, width + 8), 8, cv.CV_8UC1) 
 
     # store players moving distance
-    players = np.zeros(playerNum)
+    players = np.zeros(numOfPlayers)
 
     # store players position of last frame
     prePlayers = [] 
@@ -41,21 +50,28 @@ def main():
 
     for f in xrange(count):
         frame = cv.QueryFrame(capture)
-        
-        # find players
-        prePlayers = getplayers(curFrame) 
 
+        # find players
+        #Convert to gray
+        cv.CvtColor(frame, curFrame, cv.CV_BGR2GRAY) 
+        prePlayers = fakePlayer()
+        
         #Calculate the movement using the previous and the current frame using the previous points
         curPlayers, status, err = cv.CalcOpticalFlowPyrLK(preFrame, curFrame, prePyr, curPyr, prePlayers, (10, 10), 3, (cv.CV_TERMCRIT_ITER|cv.CV_TERMCRIT_EPS,20, 0.03), 0)
 
+        ###temp = frame
         # add new distance to list
-        for i in range(playerNum):
+        for i in range(numOfPlayers):
             players[i] += getDistance(prePlayers[i], curPlayers[i])
-           
+            ###cv.Line(temp, (int(prePlayers[i][0]), int(prePlayers[i][1])), (int(curPlayers[i][0]), int(curPlayers[i][1])), (255,122,122),3)
+
+        ###cv.ShowImage("test", temp)
+        ###cv2.waitKey(20)
+        
         #Put the current frame preFrame 
         cv.Copy(curFrame, preFrame) 
         prePlayers = curPlayers
-
+    ###cv2.destroyAllWindows()
     # print distance
     for player in players:
         print "player running distance: ", player, "\n"
