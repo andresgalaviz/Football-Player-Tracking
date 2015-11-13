f055239d30f76c5942ed01eba6fa5110b3eb6f4f
rongli1101 <rongli1101@gmail.com>
2015-11-14 01:20:10 +0800

    Update stitch.py

--- stitch.py
+++ stitch.py
@@ -3,29 +3,28 @@ import cv2
 import cv2.cv as cv
 import numpy as np
 from matplotlib import pyplot as plt
-os.chdir("D:/rongliWork/")
 
 def findHomoMatrix(img1, img2):
     sift=cv2.SIFT()
     point1, descriptor1=sift.detectAndCompute(img1,None)
     point2, descriptor2 =sift.detectAndCompute(img2,None)
 
-    FLANN_INDEX_KDTREE = 0
-    index_params = {'algorithm': FLANN_INDEX_KDTREE, 'trees': 5}
-    search_params = {'checks': 50}
+    flannIndexKDTree = 0
+    indexParam = {'algorithm': flannIndexKDTree, 'trees': 5}
+    searchParam = {'checks': 50}
 
-    flann = cv2.FlannBasedMatcher(index_params, search_params)
+    flann = cv2.FlannBasedMatcher(indexParam, searchParam)
 
     matches = flann.knnMatch(descriptor1,descriptor2,k=2)
 
-    # store all the good matches as per Lowe's ratio test.
-    good = []
-    for m,n in matches:
-        if m.distance < 0.7*n.distance:
-            good.append(m)
+    # keep good matches of points
+    goodMatches = []
+    for x,y in matches:
+        if x.distance < 0.7*y.distance:
+            goodMatches.append(x)
 
-    srcPoints = np.float32([ point1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)#cordinates of the points
-    dstPoints = np.float32([ point2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
+    srcPoints = np.float32([ point1[x.queryIdx].pt for x in goodMatches ]).reshape(-1,1,2)#cordinates of the points
+    dstPoints = np.float32([ point2[x.trainIdx].pt for x in goodMatches ]).reshape(-1,1,2)
 
     M, mask = cv2.findHomography(srcPoints, dstPoints, cv2.RANSAC,5.0)
     print('homography matrix found:')
@@ -33,21 +32,22 @@ def findHomoMatrix(img1, img2):
     return M
 
 def warpTwoImages(img1, img2, H):
-    '''warp img2 to img1 with homograph H'''
-    h1,w1 = img1.shape[:2]
-    h2,w2 = img2.shape[:2]
-    pts1 = np.float32([[0,0],[0,h1],[w1,h1],[w1,0]]).reshape(-1,1,2)
-    pts2 = np.float32([[0,0],[0,h2],[w2,h2],[w2,0]]).reshape(-1,1,2)
-    pts2_ = cv2.perspectiveTransform(pts2, H)
-    pts = np.concatenate((pts1, pts2_), axis=0)
-    [xmin, ymin] = np.int32(pts.min(axis=0).ravel() - 0.5)
-    [xmax, ymax] = np.int32(pts.max(axis=0).ravel() + 0.5)
-    t = [-xmin,-ymin]
-    Ht = np.array([[1,0,-xmin],[0,1,-ymin],[0,0,1]]) # translate
+    '''warp img2 to img1 with homograph matrix H'''
+    height1,width1 = img1.shape[:2]
+    height2,width2 = img2.shape[:2]
+    p1 = np.float32([[0,0],[0,height1],[width1,height1],[width1,0]]).reshape(-1,1,2)
+    p2 = np.float32([[0,0],[0,height2],[width2,height2],[width2,0]]).reshape(-1,1,2)
+    p3 = cv2.perspectiveTransform(p2, H)
 
-    result = cv2.warpPerspective(img2, Ht.dot(H), (xmax-xmin, ymax-ymin))
-    result[-ymin:h1-ymin,-xmin:w1-xmin] = img1
-    return result
+    #combint two images and reshape the image size
+    points = np.concatenate((p1, p3), axis=0)
+    [xmin, ymin] = np.int32(points.min(axis=0).ravel() - 0.5)
+    [xmax, ymax] = np.int32(points.max(axis=0).ravel() + 0.5)
+    M = np.array([[1,0,-xmin],[0,1,-ymin],[0,0,1]])
+
+    outputImg = cv2.warpPerspective(img2, M.dot(H), (xmax-xmin, ymax-ymin))
+    outputImg[-ymin:height1-ymin,-xmin:width1-xmin] = img1
+    return outputImg
 
 
 #step1: capture the video and get the frames
@@ -77,7 +77,7 @@ leftAndMid=warpTwoImages(img2,img1,M1)
 #crop the resulting image because its out of the field, not of our interest
 width1=len(leftAndMid)
 length1=len(leftAndMid[1,:])
-img4=leftAndMid[100:(5*width1/9),(length1/10):]#img4=rightAndMid[(width/8):(7*width/13),1:(17*length/20)]
+img4=leftAndMid[100:(5*width1/9),(length1/10):]#remove unnecessary edges of the frames.
 
 #step 4: find homography matrix for left image and rightAndMid, and do the stitching.
 M2=findHomoMatrix(img3,img4)
@@ -86,7 +86,7 @@ panorama=warpTwoImages(img4,img3,M2)
 #step5: crop the panorama image discard unintersting pixels
 width2 = len(panorama)
 length2 = len(panorama[1,:])
-panorama=panorama[(width2/8):(4*width2/7),1:19*length2/20]
+panorama=panorama[(width2/8):(4*width2/7),1:19*length2/20]#crop the picture because otherwise it will be too big for later process.
 
 #step6: write video
 panorama=cv2.resize(panorama,(0,0),fx=0.4,fy=0.4)
@@ -98,7 +98,7 @@ video = cv2.VideoWriter('panoramaFull1.avi',fourcc=-1,fps=24,frameSize=(width,he
 video.write(panorama)
 
 #do the same to all the 7199 remaining frames
-for x in range(0, 7199):
+for x in range(0, frameCount):
 
     if not cap1.isOpened():
         print('cannot open video 1')
